c3181fc07dbc0635f7be07ea8884916da0108ffc
Fatima Alondra Reyes Lopez <reyeslopezal21@gmail.com>
2024-11-11 19:08:52 -0600

    "Entrega final pizzeria"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TablaPedidosComponent } from './components/tabla-pedido/tabla-pedido.component';
+import { FormVentasComponent } from './components/form-ventas/form-ventas.component';
+import { ResumenVentasComponent } from './components/resumen-ventas/resumen-ventas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    TablaPedidosComponent,
+    FormVentasComponent,
+    ResumenVentasComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pizzeria';
